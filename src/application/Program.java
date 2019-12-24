64e0a8c4a66702bce98f9e1b4bd7585163b8b23f
Rafael De Luca <deluca1712@gmail.com>
2019-12-24 18:37:03 -0300

    Using transaction verification

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ import java.sql.Statement;
 import java.util.Locale;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
@@ -20,26 +21,53 @@ public class Program {
 		try {
 
 			conn = DB.getConnection();
+			
+			conn.setAutoCommit(false);
 			st = conn.createStatement();
 
-			int rows1 = st.executeUpdate("UPDATE seller " + "SET BaseSalary=3999.99 " + "WHERE departmentId = 1");
+			int rows1 = st.executeUpdate("UPDATE seller " + "SET BaseSalary=4200.00 " + "WHERE departmentId = 1");
 
 			// Force a false error
-
+			/*
 			int x = 0;
 			if (x < 1) {
-				throw new SQLException("Fake error");
+				throw new SQLException("Fake error");					
 			}
-			int rows2 = st.executeUpdate("UPDATE seller " + "SET BaseSalary=7400.99 " + "WHERE departmentId = 2");
+			*/
+			
+			int rows2 = st.executeUpdate("UPDATE seller " + "SET BaseSalary=8400.00 " + "WHERE departmentId = 2");
+			
+			int rows3 = st.executeUpdate("UPDATE seller " + "SET BaseSalary=7000.00 " + "WHERE Name ='Juliana da Silva' ");
 
+			conn.commit();
+			
 			System.out.println("rows1= " + rows1);
 			System.out.println("rows2= " + rows2);
+			System.out.println("rows2= " + rows3);
+			
+			System.out.println();
+
+			res = st.executeQuery("SELECT Name, BaseSalary, DepartmentID " + "FROM seller WHERE DepartmentId = 1 OR 2 OR 3");
+
+			while (res.next()) {
+
+				System.out.println("Updated Salarys [" + res.getString("Name") + ", "
+						+ String.format("U$ %.2f", res.getDouble("BaseSalary")) + "]");
+			}
 
 		}
 
 		catch (SQLException error) {
 
-			error.printStackTrace();
+			try {
+				conn.rollback();
+				throw new DbException("Transaction rolled back because "+error.getMessage());
+			}
+			
+			catch (SQLException error2) {
+				throw new DbException("Erros trying to rollback. Caused by: "+ error2.getMessage());
+				
+			}
 
 		}
 
